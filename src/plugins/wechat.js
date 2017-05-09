bc28d3531620cffb704680e552881a9dc77b3dc1
黄嘉理 <viyayaya@huangjialideMacBook-Pro.local>
2017-05-09 23:25:23 +0800

    完善微信getOpenID方法

--- src/plugins/wechat.js
+++ src/plugins/wechat.js
@@ -1,24 +1,34 @@
+let WX = null
+
 const BASE = (() => {
   switch (process.env.CODE_ENV) {
     case 'production':
       return {
-        'url': '//tmallapi.bluemoon.com.cn/wechat/',
-        'appId': 'wxb747aab3eab1a0cb'
+        'url': '//mallapi.bluemoon.com.cn/wechat/',
+        'appId': 'wx547eeee78eb998f9'
       }
     default:
       return {
         'url': '//mallapi.bluemoon.com.cn/wechat/',
-        'appId': 'wx547eeee78eb998f9'
+        'appId': 'wxb747aab3eab1a0cb'
       }
   }
 })()
+
 const TAG = document.createElement('script')
 TAG.src = 'https://res.wx.qq.com/open/js/jweixin-1.0.0.js'
-TAG.onload = ev => {
+TAG.onload = () => {
+  WX = window.wx
 }
 document.body.appendChild(TAG)
+
 export default {
+  wx: WX,
   getOpenID: () => {
-    window.location.href = BASE['url'] + 'authorization?appId=' + BASE['appId'] + '&redirect_uri=' + encodeURIComponent(window.location.href) + '&scope=snsapi_base'
+    if (/[&?]openId=/.test(window.location.search)) {
+      return decodeURIComponent(/[&?]openId=(.*?)(&|$)/.exec(window.location.search)[1])
+    } else {
+      window.location.href = BASE['url'] + 'authorization?appId=' + BASE['appId'] + '&redirect_uri=' + encodeURIComponent(window.location.href) + '&scope=snsapi_base'
+    }
   }
 }
