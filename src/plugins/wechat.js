d0a93ab92c6a02835245a480750a4c8608b1a5b8
ViYaYaYa <eureka-studio@qq.com>
2017-05-10 17:28:56 +0800

    持续优化微信配置

--- src/plugins/wechat.js
+++ src/plugins/wechat.js
@@ -1,39 +1,60 @@
-// 微信SDK全局对象
-let WX = null
+import Axios from 'axios'
+
 // 基础配置
-const BASE = (() => {
+let BASE = (() => {
+  let config = {}
   switch (process.env.CODE_ENV) {
     case 'production':
-      return {
-        'url': '//mallapi.bluemoon.com.cn/wechat/',
-        'appID': 'wx547eeee78eb998f9'
-      }
+      config['url'] = '//mallapi.bluemoon.com.cn/wechat/'
+      config['appID'] = 'wx547eeee78eb998f9'
+      break
     default:
-      return {
-        'url': '//tmallapi.bluemoon.com.cn/wechat/',
-        'appID': 'wxb747aab3eab1a0cb' // 华哥测试用的公众号
-      }
+      config['url'] = '//tmallapi.bluemoon.com.cn/wechat/'
+      config['appID'] = 'wxb747aab3eab1a0cb' // 华哥测试用的公众号
   }
+  return config
 })()
+
+// API
+let WECHAT_API = Axios.create({
+  baseURL: BASE['url'],
+  headers: { 'appId': BASE['appID'] }
+})
+WECHAT_API.interceptors.response.use(res => {
+  if (+res.data['responseCode'] === 0) {
+    return res.data
+  } else {
+    return Promise.reject(res.data)
+  }
+}, err => {
+  return Promise.reject(err)
+})
+
+// 微信SDK全局对象
+let WX = null
 // SDK的script标签
-const TAG = document.createElement('script')
-TAG.src = 'https://res.wx.qq.com/open/js/jweixin-1.0.0.js'
+let TAG = document.createElement('script')
 TAG.onload = () => {
   WX = window.wx
 }
+TAG.src = 'https://res.wx.qq.com/open/js/jweixin-1.0.0.js'
 document.body.appendChild(TAG)
+
 // 输出
 export default {
-  wx: () => {
+  wx () {
     return WX
   },
-  isWx: /MicroMessenger/i.test(window.navigator.userAgent),
-  getOpenID: ({ url, appID } = { url: BASE['url'], appID: BASE['appID'] }) => {
+  isWx () {
+    return /MicroMessenger/i.test(window.navigator.userAgent)
+  },
+  getOpenID ({ url, appID } = { url: BASE['url'], appID: BASE['appID'] }) {
     if (/\?(.*&)?openid=/i.test(window.location.href)) {
       return decodeURIComponent(/\?(.*&)?openid=(.*?)(&|$)/i.exec(window.location.href)[2])
     } else {
       window.location.href = url + 'authorization?appId=' + appID + '&redirect_uri=' + encodeURIComponent(window.location.href) + '&scope=snsapi_base'
     }
   },
-  initWxShare: () => {}
+  initWxShare () {
+  }
 }
